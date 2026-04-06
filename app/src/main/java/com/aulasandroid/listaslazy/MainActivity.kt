69bfde4831a1f1750c8b2edfe841d7fe060655b9
Guilherme Moreira <guilherme.moreira1108@gmail.com>
2026-04-06 08:17:44 -0300

    testando LazyVerticalGrid

--- app/src/main/java/com/aulasandroid/listaslazy/MainActivity.kt
+++ app/src/main/java/com/aulasandroid/listaslazy/MainActivity.kt
@@ -14,6 +14,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -113,6 +116,17 @@ fun GameScreen(modifier: Modifier = Modifier) {
                 GameCard(it)
             }
         }
+
+//        LazyVerticalGrid(
+//            columns = GridCells.Adaptive(50.dp)
+//        ) {
+//            items(gamesList) {
+//                StudioCard(it)
+//            }
+//        }
+
+
+
     }
 }
 
